fc2c010686eaf23ce47454601d27b75934c630fc
matthewegregory <matthew.e.gregory@gmail.com>
2024-04-11 16:38:01 +0100

    update devise and homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def home
+    render 'layouts/home'
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :username, :first_name, :last_name, :location, :birthdate, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password, :username, :first_name, :last_name, :location, :birthdate, :bio])
+  end
+
 end
---
+++ db/migrate/20240411145348_add_columns_to_user_model.rb
@@ -0,0 +1,10 @@
+class AddColumnsToUserModel < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :location, :string
+    add_column :users, :birthdate, :date
+    add_column :users, :bio, :text
+  end
+end
